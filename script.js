8add63c7313b6974acb85558a5c98f0d42e07593
haydeeMugisha <haydee.mugisha@hicoders.ch>
2023-12-11 01:03:48 +0100

    added class logic to add,edit and remove

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ const contentMap = {
         `,
     },
     classes: {
-        mainContent: '<h2>Classes Information</h2><p>Classes content goes here.</p>',
+        mainContent: 
+        `<h2>Classes Information</h2><p>Classes content goes here.</p>
+        <button class="btn btn-primary mt-3" onclick="addNewClass()">Add New Class</button>
+        `
+        ,
     },
     teachers: {
         mainContent: '<h2>Teachers Information</h2><p>Teachers content goes here.</p>',
@@ -33,3 +37,69 @@ function showContent(contentId) {
     document.querySelector('.container').innerHTML = content.mainContent;
 }
 
+//Function to add a new class
+function addNewClass() {
+    const className = prompt('Enter Class Name:');
+    const teacherName = prompt('Enter Teacher Name:');
+    const description = prompt('Enter Class Description:');
+
+    if (className && teacherName && description) {
+        //new box for the added class
+        const newClassBox = document.createElement('div');
+        newClassBox.className = 'box text-center class-box';
+
+        // Content of the new class box
+        newClassBox.innerHTML = `
+            <h3>${className}</h3>
+            <p>Teacher: ${teacherName}</p>
+            <p>${description}</p>
+            <div class="options">
+                <i class="bi bi-pencil" onclick="editClass(this)"></i>
+                <i class="bi bi-trash" onclick="removeClass(this)"></i>
+            </div>
+        `;
+
+        // Append the new class box to the container
+        document.querySelector('#container1').appendChild(newClassBox);
+    }
+}
+
+// Function to edit a class
+function editClass(iconElement) {
+    const classBox = iconElement.closest('.class-box');
+    const className = prompt('Enter New Class Name:');
+    const teacherName = prompt('Enter New Teacher Name:');
+    const description = prompt('Enter New Class Description:');
+
+    if (className && teacherName && description) {
+        // the content of the class box
+        classBox.innerHTML = `
+            <h3>${className}</h3>
+            <p>Teacher: ${teacherName}</p>
+            <p>${description}</p>
+            <div class="options">
+                <i class="bi bi-pencil" onclick="editClass(this)"></i>
+                <i class="bi bi-trash" onclick="removeClass(this)"></i>
+            </div>
+        `;
+    }
+}
+
+// Function to remove a class
+function removeClass(iconElement) {
+    const classBox = iconElement.closest('.class-box');
+    classBox.remove();
+}
+//to do tomorrow:
+//class  section:
+//replace prompt by modal
+//rearrage the add btn position and icon
+//style the new added division in class section
+//the edit,remove icons should be on the top right of the box
+//insure the display position of the nbr 2 class,3
+//below the student and teachers links in the box.
+
+//home section:
+//update the home page by adding the class numbers
+
+//clean the codes(to be reusable)
